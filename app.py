2833700d7a809767d98d861b16a1e0a8a37629ef
Richardaeld <Richardaeld@gmail.com>
2021-12-09 01:44:04 -0500

    Added imports and if env.py (left commented out) for MongoDB

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
+import datetime
 from flask import (
     Flask, url_for, render_template,
     redirect, request, session, flash)
 import os
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
+# from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
-
+# if os.path.exists("env.py"):
+    # import env
 
 app = Flask(__name__)
 
 # mongo envs
-'''
+"""
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-'''
+"""
 
 mongo = PyMongo(app)
 
