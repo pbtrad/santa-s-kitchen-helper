87ad587c48e9193e95716b5c1c5e15c3e4d87d1a
Alex Ciobanu <tsopi10@gmail.com>
2021-12-12 18:19:26 +0200

    Add email validation function to script.js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,31 @@
+document.getElementById("login-form").addEventListener("change", validationStep);
+document.getElementById("login-form").addEventListener("submit", validationStep);
+
+/**
+ * Starts the validation for the form
+ * @param {Automatically added} event 
+ */
+function validationStep(event) {
+    event.preventDefault();
+
+    let emailField = document.getElementById("InputEmail");
+    let firstCheck = emailValidation(emailField);
+    let secondCheck = passwordValidation(passwordField)
+}
+
+function emailValidation(email) {
+    let regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    if (email.value.trim() === ""){
+        document.getElementById("email-validation-message").innerHTML = "Email cannot be empty.";
+        document.getElementById("InputEmail").style.border = "2px solid red";
+    } else if (!regEmail.test(email.value)){
+        document.getElementById("email-validation-message").innerHTML = "Please provide a valid email.";
+        document.getElementById("InputEmail").style.border = "2px solid red";
+    } else if (regEmail.test(email.value)){
+        document.getElementById("email-validation-message").innerHTML = "";
+        document.getElementById("InputEmail").style.border = "2px solid green";
+    }
+}
 
 const showEdit = () => {
     let form = document.getElementById('edit-profile-form');
