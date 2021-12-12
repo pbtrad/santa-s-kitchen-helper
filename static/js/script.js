75416114d8898676254ee389c19b1bfb0fe974a5
Richardaeld <Richardaeld@gmail.com>
2021-12-12 13:59:14 -0500

    Added js for added multiple dishes at once

--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,40 @@
-document.getElementById("FORMIDHERE").addEventListener("submit", validationStep);
-document.getElementById("EMAILFIELDIDHERE").addEventListener("change", emailValidation);
-document.getElementById("PASSWORDFIELDIDHERE").addEventListener("change", passwordValidation);
+// document.getElementById("FORMIDHERE").addEventListener("submit", validationStep);
+// document.getElementById("EMAILFIELDIDHERE").addEventListener("change", emailValidation);
+// document.getElementById("PASSWORDFIELDIDHERE").addEventListener("change", passwordValidation);
 
-function emailValidation() {
-    email = document.getElementById("EMAILFIELDIDHERE");
-    let regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-    if (email.value.trim() === ""){
-        document.getElementById("email-validation-message").innerHTML = "Email cannot be empty.";
-        document.getElementById("EMAILFIELDIDHERE").style.border = "2px solid red";
-    } else if (!regEmail.test(email.value)){
-        document.getElementById("email-validation-message").innerHTML = "Please provide a valid email.";
-        document.getElementById("EMAILFIELDIDHERE").style.border = "2px solid red";
-    } else if (regEmail.test(email.value)){
-        document.getElementById("email-validation-message").innerHTML = " ";
-        document.getElementById("EMAILFIELDIDHERE").style.border = "2px solid green";
-    }
-}
+// function emailValidation() {
+//     email = document.getElementById("EMAILFIELDIDHERE");
+//     let regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+//     if (email.value.trim() === ""){
+//         document.getElementById("email-validation-message").innerHTML = "Email cannot be empty.";
+//         document.getElementById("EMAILFIELDIDHERE").style.border = "2px solid red";
+//     } else if (!regEmail.test(email.value)){
+//         document.getElementById("email-validation-message").innerHTML = "Please provide a valid email.";
+//         document.getElementById("EMAILFIELDIDHERE").style.border = "2px solid red";
+//     } else if (regEmail.test(email.value)){
+//         document.getElementById("email-validation-message").innerHTML = " ";
+//         document.getElementById("EMAILFIELDIDHERE").style.border = "2px solid green";
+//     }
+// }
 
-function passwordValidation(password) {
-    password = document.getElementById("");
-    let regPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    if (email.value.trim() === ""){
-        document.getElementById("pass-validation-message").innerHTML = "Password cannot be empty.";
-        document.getElementById("").style.border = "2px solid red";
-    } else if (!regPass.test(email.value)){
-        document.getElementById("pass-validation-message").innerHTML = "Password must have more than 8 letters and at least 1 number. Acceptable: a-z, A-Z, 0-9";
-        document.getElementById("").style.border = "2px solid red";
-    } else if (regPass.test(email.value)){
-        document.getElementById("pass-validation-message").innerHTML = " ";
-        document.getElementById("InputEmail").style.border = "2px solid green";
-    }
-}
+// function passwordValidation(password) {
+//     password = document.getElementById("");
+//     let regPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+//     if (email.value.trim() === ""){
+//         document.getElementById("pass-validation-message").innerHTML = "Password cannot be empty.";
+//         document.getElementById("").style.border = "2px solid red";
+//     } else if (!regPass.test(email.value)){
+//         document.getElementById("pass-validation-message").innerHTML = "Password must have more than 8 letters and at least 1 number. Acceptable: a-z, A-Z, 0-9";
+//         document.getElementById("").style.border = "2px solid red";
+//     } else if (regPass.test(email.value)){
+//         document.getElementById("pass-validation-message").innerHTML = " ";
+//         document.getElementById("InputEmail").style.border = "2px solid green";
+//     }
+// }
 
-const showEdit = () => {
-    let form = document.getElementById('edit-profile-form');
-    form.classList.toggle('hide');
-}
+// const showEdit = () => {
+//     let form = document.getElementById('edit-profile-form');
+//     form.classList.toggle('hide');
+// }
 
 
