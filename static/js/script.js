cf50d5a956590846c08e2a5376b8493c379c08b3
Alex Ciobanu <tsopi10@gmail.com>
2021-12-12 20:47:45 +0200

    Complete validation for name, email and password

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,28 @@
-document.getElementById("registration-form").addEventListener("submit", validationStep);
+// document.getElementById("registration-form").addEventListener("submit", validationStep);
 document.getElementById("InputEmail").addEventListener("change", emailValidationStep);
 document.getElementById("InputPassword").addEventListener("change", passwordValidationStep);
+document.getElementById("inputUserName").addEventListener("change", usernameValidationStep);
+
+function usernameValidationStep(event) {
+    event.preventDefault();
+
+    usernameField = document.getElementById("inputUserName");
+    let usernameCheck = usernameValidation(usernameField);
+}
+
+function usernameValidation(username) {
+    let regUser = /^(?=.*[a-z])(?=.*[A-Z]).{5,15}$/;
+    if (username.value.trim() === ""){
+        document.getElementById("username-validation-field").innerHTML = "Username cannot be empty.";
+        document.getElementById("inputUserName").style.border = "2px solid red";
+    } else if (!regUser.test(username.value)){
+        document.getElementById("username-validation-field").innerHTML = "Username must have between 5-15 letters. Acceptable: a-z, A-Z";
+        document.getElementById("inputUserName").style.border = "2px solid red";
+    } else if (regUser.test(username.value)){
+        document.getElementById("username-validation-field").innerHTML = " ";
+        document.getElementById("inputUserName").style.border = "2px solid green";
+    }
+}
 
 function emailValidationStep(event) {
     event.preventDefault();
@@ -32,7 +54,7 @@ function passwordValidationStep(event) {
     event.preventDefault();
 
     passwordField = document.getElementById("InputPassword");
-    let emailCheck = emailValidation(emailField);
+    let passwordCheck = passwordValidation(passwordField);
 }
 /**
  * Gets password from password field
@@ -41,13 +63,13 @@ function passwordValidationStep(event) {
  */
 function passwordValidation(password) {
     let regPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    if (email.value.trim() === ""){
+    if (password.value.trim() === ""){
         document.getElementById("password-validation-field").innerHTML = "Password cannot be empty.";
         document.getElementById("InputPassword").style.border = "2px solid red";
-    } else if (!regPass.test(email.value)){
+    } else if (!regPass.test(password.value)){
         document.getElementById("password-validation-field").innerHTML = "Password must have more than 8 letters and at least 1 number. Acceptable: a-z, A-Z, 0-9";
         document.getElementById("InputPassword").style.border = "2px solid red";
-    } else if (regPass.test(email.value)){
+    } else if (regPass.test(password.value)){
         document.getElementById("password-validation-field").innerHTML = " ";
         document.getElementById("InputPassword").style.border = "2px solid green";
     }
